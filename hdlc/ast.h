3035fe3afe91f1bfd492ea60daf37c117caeaae9
Artem Alekseev <fexolm@gmail.com>
2021-12-25 17:22:42 +0300

    Fix clang format

--- hdlc/ast.h
+++ hdlc/ast.h
@@ -6,8 +6,7 @@
 #include <string>
 #include <vector>
 
-namespace ast
-{
+namespace ast {
 
 struct Chip;
 struct ChipSignature;
@@ -25,220 +24,182 @@ struct RegInit;
 struct RegWrite;
 struct RegRead;
 
-struct Node
-{
-    virtual void visit(Visitor& v) = 0;
+struct Node {
+  virtual void visit(Visitor &v) = 0;
 };
 
-struct Visitor
-{
-    virtual void visit(Package& pkg) = 0;
-    virtual void visit(Chip& chip) = 0;
-    virtual void visit(AssignStmt& stmt) = 0;
-    virtual void visit(CallExpr& expr) = 0;
-    virtual void visit(Value& val) = 0;
-    virtual void visit(RetStmt& stmt) = 0;
-    virtual void visit(RegInit& reg) = 0;
-    virtual void visit(RegWrite& rw) = 0;
-    virtual void visit(RegRead& rr) = 0;
+struct Visitor {
+  virtual void visit(Package &pkg) = 0;
+  virtual void visit(Chip &chip) = 0;
+  virtual void visit(AssignStmt &stmt) = 0;
+  virtual void visit(CallExpr &expr) = 0;
+  virtual void visit(Value &val) = 0;
+  virtual void visit(RetStmt &stmt) = 0;
+  virtual void visit(RegInit &reg) = 0;
+  virtual void visit(RegWrite &rw) = 0;
+  virtual void visit(RegRead &rr) = 0;
 };
 
-struct Package : Node
-{
-    std::string name;
-    std::vector<std::shared_ptr<Chip>> chips;
+struct Package : Node {
+  std::string name;
+  std::vector<std::shared_ptr<Chip>> chips;
 
-    void visit(Visitor& v) override { v.visit(*this); };
+  void visit(Visitor &v) override { v.visit(*this); };
 };
 
-struct Chip : Node
-{
-    std::string ident;
-    std::vector<std::shared_ptr<Value>> inputs;
-    std::vector<std::shared_ptr<Type>> outputs;
-    std::vector<std::shared_ptr<Stmt>> body;
+struct Chip : Node {
+  std::string ident;
+  std::vector<std::shared_ptr<Value>> inputs;
+  std::vector<std::shared_ptr<Type>> outputs;
+  std::vector<std::shared_ptr<Stmt>> body;
 
-    void visit(Visitor& v) override { v.visit(*this); }
+  void visit(Visitor &v) override { v.visit(*this); }
 };
 
-struct Type
-{
-    std::string name;
+struct Type {
+  std::string name;
 
-    explicit Type(const std::string& name) : name(name) {}
+  explicit Type(const std::string &name) : name(name) {}
 };
 
-struct Stmt : Node
-{
+struct Stmt : Node {};
+
+struct AssignStmt : Stmt {
+  std::vector<std::shared_ptr<Value>> assignees;
+  std::shared_ptr<Expr> rhs;
+
+  void visit(Visitor &v) override { v.visit(*this); }
 };
 
-struct AssignStmt : Stmt
-{
-    std::vector<std::shared_ptr<Value>> assignees;
-    std::shared_ptr<Expr> rhs;
+struct Expr : Node {};
 
-    void visit(Visitor& v) override { v.visit(*this); }
+struct CallExpr : Expr {
+  std::string chip_name;
+  std::vector<std::shared_ptr<Expr>> args;
+
+  CallExpr(std::string name, std::vector<std::shared_ptr<Expr>> args)
+      : chip_name(name), args(args) {}
+
+  void visit(Visitor &v) override { v.visit(*this); }
 };
 
-struct Expr : Node
-{
+struct Value : Expr {
+  std::string ident;
+  std::shared_ptr<Type> type;
+
+  Value(std::string name, std::shared_ptr<Type> type)
+      : ident(name), type(type) {}
+
+  Value() {}
+
+  void visit(Visitor &v) override { v.visit(*this); }
 };
 
-struct CallExpr : Expr
-{
-    std::string chip_name;
-    std::vector<std::shared_ptr<Expr>> args;
+struct RetStmt : Stmt {
+  std::vector<std::shared_ptr<Expr>> results;
 
-    CallExpr(std::string name, std::vector<std::shared_ptr<Expr>> args)
-        : chip_name(name), args(args)
-    {
+  void visit(Visitor &v) override { v.visit(*this); }
+};
+
+struct RegInit : Stmt {
+  std::shared_ptr<Value> reg;
+
+  explicit RegInit(std::shared_ptr<Value> reg) : reg(reg) {}
+
+  void visit(Visitor &v) override { v.visit(*this); }
+};
+
+struct RegWrite : Stmt {
+  std::shared_ptr<Value> reg;
+  std::shared_ptr<Expr> rhs;
+
+  explicit RegWrite(std::shared_ptr<Value> reg, std::shared_ptr<Expr> rhs)
+      : reg(reg), rhs(rhs) {}
+
+  void visit(Visitor &v) override { v.visit(*this); }
+};
+
+struct RegRead : Expr {
+  std::shared_ptr<Value> reg;
+
+  explicit RegRead(std::shared_ptr<Value> reg) : reg(reg) {}
+
+  void visit(Visitor &v) override { v.visit(*this); }
+};
+
+struct Printer : Visitor {
+  std::ostream &out;
+
+  Printer(std::ostream &out) : out(out) {}
+  void visit(Package &pkg) {
+    out << "Package: " << pkg.name << "\n\n";
+    for (auto &c : pkg.chips) {
+      c->visit(*this);
     }
-
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct Value : Expr
-{
-    std::string ident;
-    std::shared_ptr<Type> type;
-
-    Value(std::string name, std::shared_ptr<Type> type)
-        : ident(name), type(type)
-    {
+  }
+  void visit(Chip &chip) {
+    out << "chip " << chip.ident << " (";
+    for (auto &i : chip.inputs) {
+      i->visit(*this);
+      out << ", ";
     }
+    out << ") ";
 
-    Value() {}
-
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct RetStmt : Stmt
-{
-    std::vector<std::shared_ptr<Expr>> results;
-
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct RegInit : Stmt
-{
-    std::shared_ptr<Value> reg;
-
-    explicit RegInit(std::shared_ptr<Value> reg) : reg(reg) {}
-
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct RegWrite : Stmt
-{
-    std::shared_ptr<Value> reg;
-    std::shared_ptr<Expr> rhs;
-
-    explicit RegWrite(std::shared_ptr<Value> reg, std::shared_ptr<Expr> rhs)
-        : reg(reg), rhs(rhs)
-    {
+    for (auto &o : chip.outputs) {
+      out << o->name << ", ";
     }
+    out << "{" << std::endl;
 
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct RegRead : Expr
-{
-    std::shared_ptr<Value> reg;
-
-    explicit RegRead(std::shared_ptr<Value> reg) : reg(reg) {}
-
-    void visit(Visitor& v) override { v.visit(*this); }
-};
-
-struct Printer : Visitor
-{
-    std::ostream& out;
-
-    Printer(std::ostream& out) : out(out) {}
-    void visit(Package& pkg)
-    {
-        out << "Package: " << pkg.name << "\n\n";
-        for (auto& c : pkg.chips)
-        {
-            c->visit(*this);
-        }
+    for (auto &s : chip.body) {
+      s->visit(*this);
+      out << std::endl;
     }
-    void visit(Chip& chip)
-    {
-        out << "chip " << chip.ident << " (";
-        for (auto& i : chip.inputs)
-        {
-            i->visit(*this);
-            out << ", ";
-        }
-        out << ") ";
+    out << "}"
+        << "\n\n";
+  }
 
-        for (auto& o : chip.outputs)
-        {
-            out << o->name << ", ";
-        }
-        out << "{" << std::endl;
+  void visit(Value &val) { out << val.ident; }
+  void visit(AssignStmt &stmt) {
+    out << "    ";
+    for (auto &v : stmt.assignees) {
+      v->visit(*this);
+      out << ", ";
+    }
+    out << ":= ";
 
-        for (auto& s : chip.body)
-        {
-            s->visit(*this);
-            out << std::endl;
-        }
-        out << "}"
-            << "\n\n";
+    stmt.rhs->visit(*this);
+  }
+  void visit(CallExpr &expr) {
+    out << expr.chip_name << "(";
+    for (auto &arg : expr.args) {
+      arg->visit(*this);
+      out << ", ";
     }
+    out << ")";
+  }
+  void visit(RetStmt &stmt) {
+    out << "    return ";
+    for (auto &v : stmt.results) {
+      v->visit(*this);
+      out << ", ";
+    }
+  }
 
-    void visit(Value& val) { out << val.ident; }
-    void visit(AssignStmt& stmt)
-    {
-        out << "    ";
-        for (auto& v : stmt.assignees)
-        {
-            v->visit(*this);
-            out << ", ";
-        }
-        out << ":= ";
+  void visit(RegInit &init) {
+    out << "    reg ";
+    init.reg->visit(*this);
+  }
 
-        stmt.rhs->visit(*this);
-    }
-    void visit(CallExpr& expr)
-    {
-        out << expr.chip_name << "(";
-        for (auto& arg : expr.args)
-        {
-            arg->visit(*this);
-            out << ", ";
-        }
-        out << ")";
-    }
-    void visit(RetStmt& stmt)
-    {
-        out << "    return ";
-        for (auto& v : stmt.results)
-        {
-            v->visit(*this);
-            out << ", ";
-        }
-    }
+  void visit(RegWrite &rw) {
+    out << "    ";
+    rw.reg->visit(*this);
+    out << " <- ";
+    rw.rhs->visit(*this);
+  }
 
-    void visit(RegInit& init)
-    {
-        out << "    reg ";
-        init.reg->visit(*this);
-    }
-
-    void visit(RegWrite& rw)
-    {
-        out << "    ";
-        rw.reg->visit(*this);
-        out << " <- ";
-        rw.rhs->visit(*this);
-    }
-
-    void visit(RegRead& rr)
-    {
-        out << "<- ";
-        rr.reg->visit(*this);
-    }
+  void visit(RegRead &rr) {
+    out << "<- ";
+    rr.reg->visit(*this);
+  }
 };
 } // namespace ast
--- hdlc/parser_error.h
+++ hdlc/parser_error.h
@@ -3,12 +3,11 @@
 #include <exception>
 #include <sstream>
 
-class ParserError : public std::exception
-{
+class ParserError : public std::exception {
 private:
-    std::string message;
+  std::string message;
 
 public:
-    ParserError(std::string msg, size_t line, size_t line_pos);
-    const char* what() const noexcept override { return message.c_str(); }
+  ParserError(std::string msg, size_t line, size_t line_pos);
+  const char *what() const noexcept override { return message.c_str(); }
 };
