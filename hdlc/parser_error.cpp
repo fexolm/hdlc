382418b5f561012fcbd35c21c567b4305d71309d
VitaliiOnufrienko <bimacka@gmail.com>
2021-12-29 14:31:24 +0300

    add parse tests

--- hdlc/parser_error.cpp
+++ hdlc/parser_error.cpp
@@ -3,6 +3,6 @@
 ParserError::ParserError(std::string msg, size_t line, size_t line_pos) {
   std::stringstream ss;
   ss << "Parser error: " << msg << " (line " << line << ", pos " << line_pos
-     << ")" << std::endl;
+     << ")";
   message = ss.str();
 }
